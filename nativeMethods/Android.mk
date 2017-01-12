64693ae3f99668d7a5cebe9262564e4491fb44df
Viren Nadkarni <viren@outlook.com>
2017-01-12 06:25:04 +0000

    Fix makefiles

--- nativeMethods/Android.mk
+++ nativeMethods/Android.mk
@@ -1,3 +1,1 @@
-# The namespace in Java file, with dots replaced with underscores
 include $(call all-subdir-makefiles)
- 
--- nativeMethods/gralloc/Android.mk
+++ nativeMethods/gralloc/Android.mk
@@ -1,17 +1,16 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES = \
-									gralloc.c
+LOCAL_SRC_FILES = gralloc.c
 
-LOCAL_C_INCLUDES +=	\
-										$(LOCAL_PATH)\
-										$(LOCAL_PATH)/..
+LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/..
 
-LOCAL_PRELINK_MODULE:=false #override prelink map
+LOCAL_PRELINK_MODULE := false
+
 LOCAL_MODULE := libdvnc_gralloc_sdk$(PLATFORM_SDK_VERSION) 
-LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(LOCAL_PATH)/../libs/$(TARGET_CPU_ABI)
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_SHARED_LIBRARIES := libhardware libcutils
 
